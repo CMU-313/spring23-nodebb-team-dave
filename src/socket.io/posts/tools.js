8100425b63ba42ac07a7174c79999588bcd235d4
zhinanw@andrew.cmu.edu <zhinanwang@znbuchangdoudoudeMBP.local>
2023-02-11 15:28:41 -0500

    Add attribute showing endorsed in postData of src/socket.io/posts/tools.js

--- src/socket.io/posts/tools.js
+++ src/socket.io/posts/tools.js
@@ -29,6 +29,7 @@ module.exports = function (SocketPosts) {
             canFlag: privileges.posts.canFlag(data.pid, socket.uid),
             flagged: flags.exists('post', data.pid, socket.uid), // specifically, whether THIS calling user flagged
             bookmarked: posts.hasBookmarked(data.pid, socket.uid),
+            endorsed: posts.hasEndorsed(data.pid),
             postSharing: social.getActivePostSharing(),
             history: posts.diffs.exists(data.pid),
             canViewInfo: privileges.global.can('view:users:info', socket.uid),
@@ -37,6 +38,7 @@ module.exports = function (SocketPosts) {
         const postData = results.posts;
         postData.absolute_url = `${nconf.get('url')}/post/${data.pid}`;
         postData.bookmarked = results.bookmarked;
+        postData.endorsed = results.endorsed
         postData.selfPost = socket.uid && socket.uid === postData.uid;
         postData.display_edit_tools = results.canEdit.flag;
         postData.display_delete_tools = results.canDelete.flag;
