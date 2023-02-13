fe54c7a6914da32a888eaaf9bcfc094c5d5f6fa3
Joanna Ge <joannage87@gmail.com>
2023-02-13 00:23:37 -0500

    Add function signature types to configs.ts

--- src/meta/configs.js
+++ src/meta/configs.js
@@ -1,22 +1,29 @@
-
-'use strict';
-
-const nconf = require('nconf');
-const path = require('path');
-const winston = require('winston');
-
-const db = require('../database');
-const pubsub = require('../pubsub');
-const plugins = require('../plugins');
-const utils = require('../utils');
-const Meta = require('./index');
-const cacheBuster = require('./cacheBuster');
-const defaults = require('../../install/data/defaults.json');
-
-const Configs = module.exports;
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cookie = exports.registerHooks = exports.remove = exports.setOnEmpty = exports.setMultiple = exports.set = exports.get = exports.init = exports.list = exports.getFields = exports.serialize = exports.deserialize = void 0;
+const nconf = require("nconf");
+const path = require("path");
+const winston = require("winston");
+const db = require("../database");
+const pubsub = require("../pubsub");
+const plugins = require("../plugins");
+const utils = require("../utils");
+const Meta = require("./index");
+const cacheBuster = require("./cacheBuster");
+const defaults = require("../../install/data/defaults.json");
 Meta.config = {};
-
+// disabled linter because dictionary is able to hold any type for value
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument,
+@typescript-eslint/no-explicit-any */
 // called after data is loaded from db
 function deserialize(config) {
     const deserialized = {};
@@ -24,37 +31,47 @@ function deserialize(config) {
         const defaultType = typeof defaults[key];
         const type = typeof config[key];
         const number = parseFloat(config[key]);
-
         if (defaultType === 'string' && type === 'number') {
             deserialized[key] = String(config[key]);
-        } else if (defaultType === 'number' && type === 'string') {
+        }
+        else if (defaultType === 'number' && type === 'string') {
             if (!isNaN(number) && isFinite(config[key])) {
                 deserialized[key] = number;
-            } else {
+            }
+            else {
                 deserialized[key] = defaults[key];
             }
-        } else if (config[key] === 'true') {
+        }
+        else if (config[key] === 'true') {
             deserialized[key] = true;
-        } else if (config[key] === 'false') {
+        }
+        else if (config[key] === 'false') {
             deserialized[key] = false;
-        } else if (config[key] === null) {
+        }
+        else if (config[key] === null) {
             deserialized[key] = defaults[key];
-        } else if (defaultType === 'undefined' && !isNaN(number) && isFinite(config[key])) {
+        }
+        else if (defaultType === 'undefined' && !isNaN(number) && isFinite(config[key])) {
             deserialized[key] = number;
-        } else if (Array.isArray(defaults[key]) && !Array.isArray(config[key])) {
+        }
+        else if (Array.isArray(defaults[key]) && !Array.isArray(config[key])) {
             try {
                 deserialized[key] = JSON.parse(config[key] || '[]');
-            } catch (err) {
-                winston.error(err.stack);
-                deserialized[key] = defaults[key];
             }
-        } else {
+            catch (err) {
+                if (err instanceof Error) {
+                    winston.error(err.stack);
+                    deserialized[key] = defaults[key];
+                }
+            }
+        }
+        else {
             deserialized[key] = config[key];
         }
     });
     return deserialized;
 }
-
+exports.deserialize = deserialize;
 // called before data is saved to db
 function serialize(config) {
     const serialized = {};
@@ -62,171 +79,178 @@ function serialize(config) {
         const defaultType = typeof defaults[key];
         const type = typeof config[key];
         const number = parseFloat(config[key]);
-
         if (defaultType === 'string' && type === 'number') {
             serialized[key] = String(config[key]);
-        } else if (defaultType === 'number' && type === 'string') {
+        }
+        else if (defaultType === 'number' && type === 'string') {
             if (!isNaN(number) && isFinite(config[key])) {
                 serialized[key] = number;
-            } else {
+            }
+            else {
                 serialized[key] = defaults[key];
             }
-        } else if (config[key] === null) {
+        }
+        else if (config[key] === null) {
             serialized[key] = defaults[key];
-        } else if (defaultType === 'undefined' && !isNaN(number) && isFinite(config[key])) {
+        }
+        else if (defaultType === 'undefined' && !isNaN(number) && isFinite(config[key])) {
             serialized[key] = number;
-        } else if (Array.isArray(defaults[key]) && Array.isArray(config[key])) {
+        }
+        else if (Array.isArray(defaults[key]) && Array.isArray(config[key])) {
             serialized[key] = JSON.stringify(config[key]);
-        } else {
+        }
+        else {
             serialized[key] = config[key];
         }
     });
     return serialized;
 }
-
-Configs.deserialize = deserialize;
-Configs.serialize = serialize;
-
-Configs.init = async function () {
-    const config = await Configs.list();
-    const buster = await cacheBuster.read();
-    config['cache-buster'] = `v=${buster || Date.now()}`;
-    Meta.config = config;
-};
-
-Configs.list = async function () {
-    return await Configs.getFields([]);
-};
-
-Configs.get = async function (field) {
-    const values = await Configs.getFields([field]);
-    return (values.hasOwnProperty(field) && values[field] !== undefined) ? values[field] : null;
-};
-
-Configs.getFields = async function (fields) {
-    let values;
-    if (fields.length) {
-        values = await db.getObjectFields('config', fields);
-    } else {
-        values = await db.getObject('config');
-    }
-
-    values = { ...defaults, ...(values ? deserialize(values) : {}) };
-
-    if (!fields.length) {
-        values.version = nconf.get('version');
-        values.registry = nconf.get('registry');
-    }
-    return values;
-};
-
-Configs.set = async function (field, value) {
-    if (!field) {
-        throw new Error('[[error:invalid-data]]');
-    }
-
-    await Configs.setMultiple({
-        [field]: value,
+exports.serialize = serialize;
+/* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument,
+@typescript-eslint/no-explicit-any */
+function getFields(fields) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let values;
+        if (fields.length) {
+            values = yield db.getObjectFields('config', fields);
+        }
+        else {
+            values = yield db.getObject('config');
+        }
+        values = Object.assign(Object.assign({}, defaults), (values ? deserialize(values) : {}));
+        if (!fields.length) {
+            values.version = nconf.get('version');
+            values.registry = nconf.get('registry');
+        }
+        return values;
     });
-};
-
-Configs.setMultiple = async function (data) {
-    await processConfig(data);
-    data = serialize(data);
-    await db.setObject('config', data);
-    updateConfig(deserialize(data));
-};
-
-Configs.setOnEmpty = async function (values) {
-    const data = await db.getObject('config');
-    values = serialize(values);
-    const config = { ...values, ...(data ? serialize(data) : {}) };
-    await db.setObject('config', config);
-};
-
-Configs.remove = async function (field) {
-    await db.deleteObjectField('config', field);
-};
-
-Configs.registerHooks = () => {
+}
+exports.getFields = getFields;
+function list() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield getFields([]);
+    });
+}
+exports.list = list;
+function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const config = yield list();
+        const buster = yield cacheBuster.read();
+        config['cache-buster'] = `v=${buster || Date.now()}`;
+        Meta.config = config;
+    });
+}
+exports.init = init;
+function get(field) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const values = yield getFields([field]);
+        return (values.hasOwnProperty(field) && values[field] !== undefined) ? values[field] : null;
+    });
+}
+exports.get = get;
+function set(field, value) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!field) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        yield setMultiple({
+            [field]: value,
+        });
+    });
+}
+exports.set = set;
+function setMultiple(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield processConfig(data);
+        data = serialize(data);
+        yield db.setObject('config', data);
+        updateConfig(deserialize(data));
+    });
+}
+exports.setMultiple = setMultiple;
+function setOnEmpty(values) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const data = yield db.getObject('config');
+        values = serialize(values);
+        const config = Object.assign(Object.assign({}, values), (data ? serialize(data) : {}));
+        yield db.setObject('config', config);
+    });
+}
+exports.setOnEmpty = setOnEmpty;
+function remove(field) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield db.deleteObjectField('config', field);
+    });
+}
+exports.remove = remove;
+function registerHooks() {
     plugins.hooks.register('core', {
         hook: 'filter:settings.set',
-        method: async ({ plugin, settings, quiet }) => {
+        method: ({ plugin, settings, quiet }) => __awaiter(this, void 0, void 0, function* () {
             if (plugin === 'core.api' && Array.isArray(settings.tokens)) {
                 // Generate tokens if not present already
                 settings.tokens.forEach((set) => {
                     if (set.token === '') {
                         set.token = utils.generateUUID();
                     }
-
                     if (isNaN(parseInt(set.uid, 10))) {
                         set.uid = 0;
                     }
                 });
             }
-
             return { plugin, settings, quiet };
-        },
+        }),
     });
-
     plugins.hooks.register('core', {
         hook: 'filter:settings.get',
-        method: async ({ plugin, values }) => {
+        method: ({ plugin, values }) => __awaiter(this, void 0, void 0, function* () {
             if (plugin === 'core.api' && Array.isArray(values.tokens)) {
                 values.tokens = values.tokens.map((tokenObj) => {
                     tokenObj.uid = parseInt(tokenObj.uid, 10);
                     if (tokenObj.timestamp) {
                         tokenObj.timestampISO = new Date(parseInt(tokenObj.timestamp, 10)).toISOString();
                     }
-
                     return tokenObj;
                 });
             }
-
             return { plugin, values };
-        },
+        }),
     });
-};
-
-Configs.cookie = {
+}
+exports.registerHooks = registerHooks;
+exports.cookie = {
     get: () => {
         const cookie = {};
-
         if (nconf.get('cookieDomain') || Meta.config.cookieDomain) {
             cookie.domain = nconf.get('cookieDomain') || Meta.config.cookieDomain;
         }
-
         if (nconf.get('secure')) {
             cookie.secure = true;
         }
-
         const relativePath = nconf.get('relative_path');
         if (relativePath !== '') {
             cookie.path = relativePath;
         }
-
         // Ideally configurable from ACP, but cannot be "Strict" as then top-level access will treat it as guest.
-        cookie.sameSite = 'Lax';
-
+        cookie.sameSite = 'lax';
         return cookie;
     },
 };
-
-async function processConfig(data) {
-    ensureInteger(data, 'maximumUsernameLength', 1);
-    ensureInteger(data, 'minimumUsernameLength', 1);
-    ensureInteger(data, 'minimumPasswordLength', 1);
-    ensureInteger(data, 'maximumAboutMeLength', 0);
-    if (data.minimumUsernameLength > data.maximumUsernameLength) {
-        throw new Error('[[error:invalid-data]]');
-    }
-
-    await Promise.all([
-        saveRenderedCss(data),
-        getLogoSize(data),
-    ]);
+function processConfig(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        ensureInteger(data, 'maximumUsernameLength', 1);
+        ensureInteger(data, 'minimumUsernameLength', 1);
+        ensureInteger(data, 'minimumPasswordLength', 1);
+        ensureInteger(data, 'maximumAboutMeLength', 0);
+        if (data['minimumUsernameLength'] > data['maximumUsernameLength']) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        yield Promise.all([
+            saveRenderedCss(data),
+            getLogoSize(data),
+        ]);
+    });
 }
-
 function ensureInteger(data, field, min) {
     if (data.hasOwnProperty(field)) {
         data[field] = parseInt(data[field], 10);
@@ -235,53 +259,54 @@ function ensureInteger(data, field, min) {
         }
     }
 }
-
-async function saveRenderedCss(data) {
-    if (!data.customCSS) {
-        return;
-    }
-    const less = require('less');
-    const lessObject = await less.render(data.customCSS, {
-        compress: true,
-        javascriptEnabled: false,
-    });
-    data.renderedCustomCSS = lessObject.css;
-}
-
-async function getLogoSize(data) {
-    const image = require('../image');
-    if (!data['brand:logo']) {
-        return;
-    }
-    let size;
-    try {
-        size = await image.size(path.join(nconf.get('upload_path'), 'system', 'site-logo-x50.png'));
-    } catch (err) {
-        if (err.code === 'ENOENT') {
-            // For whatever reason the x50 logo wasn't generated, gracefully error out
-            winston.warn('[logo] The email-safe logo doesn\'t seem to have been created, please re-upload your site logo.');
-            size = {
-                height: 0,
-                width: 0,
-            };
-        } else {
-            throw err;
+function saveRenderedCss(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!data['customCSS']) {
+            return;
         }
-    }
-    data['brand:emailLogo'] = nconf.get('url') + path.join(nconf.get('upload_url'), 'system', 'site-logo-x50.png');
-    data['brand:emailLogo:height'] = size.height;
-    data['brand:emailLogo:width'] = size.width;
+        const less = require('less');
+        const lessObject = yield less.render(data['customCSS'], {
+            compress: true,
+            javascriptEnabled: false,
+        });
+        data['renderedCustomCSS'] = lessObject.css;
+    });
 }
-
+function getLogoSize(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const image = require('../image');
+        if (!data['brand:logo']) {
+            return;
+        }
+        let size;
+        try {
+            size = yield image.size(path.join(nconf.get('upload_path'), 'system', 'site-logo-x50.png'));
+        }
+        catch (err) {
+            if (err.code === 'ENOENT') {
+                // For whatever reason the x50 logo wasn't generated, gracefully error out
+                winston.warn('[logo] The email-safe logo doesn\'t seem to have been created, please re-upload your site logo.');
+                size = {
+                    height: 0,
+                    width: 0,
+                };
+            }
+            else {
+                throw err;
+            }
+        }
+        data['brand:emailLogo'] = nconf.get('url') + path.join(nconf.get('upload_url'), 'system', 'site-logo-x50.png');
+        data['brand:emailLogo:height'] = size.height;
+        data['brand:emailLogo:width'] = size.width;
+    });
+}
 function updateConfig(config) {
     updateLocalConfig(config);
     pubsub.publish('config:update', config);
 }
-
 function updateLocalConfig(config) {
     Object.assign(Meta.config, config);
 }
-
 pubsub.on('config:update', (config) => {
     if (typeof config === 'object' && Meta.config) {
         updateLocalConfig(config);
--- src/meta/configs.ts
+++ src/meta/configs.ts
@@ -3,6 +3,7 @@ import nconf = require('nconf');
 import path = require('path');
 import winston = require('winston');
 
+import { CookieOptions } from 'express';
 import db = require('../database');
 import pubsub = require('../pubsub');
 import plugins = require('../plugins');
@@ -10,7 +11,6 @@ import utils = require('../utils');
 import Meta = require('./index');
 import cacheBuster = require('./cacheBuster');
 import defaults = require('../../install/data/defaults.json');
-import { CookieOptions } from 'express';
 
 Meta.config = {};
 
@@ -18,9 +18,12 @@ interface ConfigCookie {
   get(): CookieOptions;
 }
 
+// disabled linter because dictionary is able to hold any type for value
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument,
+@typescript-eslint/no-explicit-any */
 // called after data is loaded from db
-export function deserialize(config) {
-    const deserialized = {};
+export function deserialize(config: object): { [id: string]: any } {
+    const deserialized: { [id: string]: any } = {};
     Object.keys(config).forEach((key) => {
         const defaultType = typeof defaults[key];
         const type = typeof config[key];
@@ -45,9 +48,11 @@ export function deserialize(config) {
         } else if (Array.isArray(defaults[key]) && !Array.isArray(config[key])) {
             try {
                 deserialized[key] = JSON.parse(config[key] || '[]');
-            } catch (err) {
-                winston.error(err.stack);
-                deserialized[key] = defaults[key];
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    winston.error(err.stack);
+                    deserialized[key] = defaults[key];
+                }
             }
         } else {
             deserialized[key] = config[key];
@@ -57,8 +62,8 @@ export function deserialize(config) {
 }
 
 // called before data is saved to db
-export function serialize(config) {
-    const serialized = {};
+export function serialize(config: object): { [id: string]: any } {
+    const serialized: { [id: string]: any } = {};
     Object.keys(config).forEach((key) => {
         const defaultType = typeof defaults[key];
         const type = typeof config[key];
@@ -84,25 +89,10 @@ export function serialize(config) {
     });
     return serialized;
 }
-
-export async function init() {
-    const config = await list();
-    const buster = await cacheBuster.read();
-    config['cache-buster'] = `v=${buster || Date.now()}`;
-    Meta.config = config;
-};
-
-export async function list() {
-    return await getFields([]);
-};
-
-export async function get(field) {
-    const values = await getFields([field]);
-    return (values.hasOwnProperty(field) && values[field] !== undefined) ? values[field] : null;
-};
-
-export async function getFields(fields) {
-    let values;
+/* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument,
+@typescript-eslint/no-explicit-any */
+export async function getFields(fields: string[]): Promise<{ [key: string]: any }> {
+    let values: any;
     if (fields.length) {
         values = await db.getObjectFields('config', fields);
     } else {
@@ -116,9 +106,25 @@ export async function getFields(fields) {
         values.registry = nconf.get('registry');
     }
     return values;
-};
+}
 
-export async function set(field, value) {
+export async function list(): Promise<{ [key: string]: any }> {
+    return await getFields([]);
+}
+
+export async function init(): Promise<void> {
+    const config: { [key: string]: any } = await list();
+    const buster = await cacheBuster.read();
+    config['cache-buster'] = `v=${buster || Date.now()}`;
+    Meta.config = config;
+}
+
+export async function get(field: string): Promise<any> {
+    const values = await getFields([field]);
+    return (values.hasOwnProperty(field) && values[field] !== undefined) ? values[field] : null;
+}
+
+export async function set(field: string, value: any): Promise<void> {
     if (!field) {
         throw new Error('[[error:invalid-data]]');
     }
@@ -126,25 +132,25 @@ export async function set(field, value) {
     await setMultiple({
         [field]: value,
     });
-};
+}
 
-export async function setMultiple(data) {
+export async function setMultiple(data: object): Promise<void> {
     await processConfig(data);
     data = serialize(data);
     await db.setObject('config', data);
     updateConfig(deserialize(data));
-};
+}
 
-export async function setOnEmpty(values) {
+export async function setOnEmpty(values: object): Promise<void> {
     const data = await db.getObject('config');
     values = serialize(values);
     const config = { ...values, ...(data ? serialize(data) : {}) };
     await db.setObject('config', config);
-};
+}
 
-export async function remove(field) {
+export async function remove(field: string): Promise<void> {
     await db.deleteObjectField('config', field);
-};
+}
 
 export function registerHooks() {
     plugins.hooks.register('core', {
@@ -184,11 +190,11 @@ export function registerHooks() {
             return { plugin, values };
         },
     });
-};
+}
 
 export const cookie: ConfigCookie = {
     get: () => {
-        let cookie: CookieOptions = {};
+        const cookie: CookieOptions = {};
 
         if (nconf.get('cookieDomain') || Meta.config.cookieDomain) {
             cookie.domain = nconf.get('cookieDomain') || Meta.config.cookieDomain;
@@ -210,12 +216,21 @@ export const cookie: ConfigCookie = {
     },
 };
 
-async function processConfig(data) {
+function ensureInteger(data: object, field: string, min: number): void {
+    if (data.hasOwnProperty(field)) {
+        data[field] = parseInt(data[field], 10);
+        if (!(data[field] >= min)) {
+            throw new Error('[[error:invalid-data]]');
+        }
+    }
+}
+
+async function processConfig(data: object): Promise<void> {
     ensureInteger(data, 'maximumUsernameLength', 1);
     ensureInteger(data, 'minimumUsernameLength', 1);
     ensureInteger(data, 'minimumPasswordLength', 1);
     ensureInteger(data, 'maximumAboutMeLength', 0);
-    if (data.minimumUsernameLength > data.maximumUsernameLength) {
+    if (data['minimumUsernameLength'] > data['maximumUsernameLength']) {
         throw new Error('[[error:invalid-data]]');
     }
 
@@ -225,28 +240,19 @@ async function processConfig(data) {
     ]);
 }
 
-function ensureInteger(data, field, min) {
-    if (data.hasOwnProperty(field)) {
-        data[field] = parseInt(data[field], 10);
-        if (!(data[field] >= min)) {
-            throw new Error('[[error:invalid-data]]');
-        }
-    }
-}
-
-async function saveRenderedCss(data) {
-    if (!data.customCSS) {
+async function saveRenderedCss(data: object): Promise<void> {
+    if (!data['customCSS']) {
         return;
     }
     const less = require('less');
-    const lessObject = await less.render(data.customCSS, {
+    const lessObject = await less.render(data['customCSS'], {
         compress: true,
         javascriptEnabled: false,
     });
-    data.renderedCustomCSS = lessObject.css;
+    data['renderedCustomCSS'] = lessObject.css;
 }
 
-async function getLogoSize(data) {
+async function getLogoSize(data: { [key: string]: any }): Promise<void> {
     const image = require('../image');
     if (!data['brand:logo']) {
         return;
@@ -271,16 +277,16 @@ async function getLogoSize(data) {
     data['brand:emailLogo:width'] = size.width;
 }
 
-function updateConfig(config) {
+function updateConfig(config): void {
     updateLocalConfig(config);
     pubsub.publish('config:update', config);
 }
 
-function updateLocalConfig(config) {
+function updateLocalConfig(config): void {
     Object.assign(Meta.config, config);
 }
 
-pubsub.on('config:update', (config) => {
+pubsub.on('config:update', (config: object) => {
     if (typeof config === 'object' && Meta.config) {
         updateLocalConfig(config);
     }
