d35e7e28e58e8fc343e71898cc63157591cd3799
Frank <fsacco@andrew.cmu.edu>
2023-03-15 17:52:15 -0400

    added some really poorly formatted code

--- example_code_change.js
+++ example_code_change.js
@@ -11,3 +11,19 @@ const result = addNumbers(5, 10);
 
 // Print the result to the console
 console.log(result);
+
+
+//Beginning of badly formatted code!
+function subtractNumbers(num1,num2) { //Notice the comma is not spaced
+  if(1==1)
+    return num1-num2;
+  else
+    return 3;
+}
+
+//Poorly spaced code
+const
+a
+=
+2
+;
