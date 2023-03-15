61a89ccb2cdc175df061cf9fd02a8404d33cb77c
Frank <fsacco@andrew.cmu.edu>
2023-03-15 17:39:09 -0400

    created example_code_change.js with some super cool code! I am formatting this commit in a really long line which is terrible practice! Lorem Ipsum!

---
+++ example_code_change.js
@@ -0,0 +1,13 @@
+//Woww cool javascript code
+//Here is some filler courtesy chatGPT
+
+// Define a function that adds two numbers
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+// Call the function and assign the result to a variable
+const result = addNumbers(5, 10);
+
+// Print the result to the console
+console.log(result);
