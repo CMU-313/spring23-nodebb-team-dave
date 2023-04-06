1c5e21edda0f1f81336be73de22019016bb9527c
Masnan <zhinanwang@znbuchangdoudoudeMBP.local>
2023-04-06 00:09:30 -0400

    set up middleware and router for NodeBB endpoint of API requests

--- src/controllers/write/index.js
+++ src/controllers/write/index.js
@@ -2,13 +2,14 @@
 
 const Write = module.exports
 
-Write.users = require('./users')
-Write.groups = require('./groups')
-Write.categories = require('./categories')
-Write.topics = require('./topics')
-Write.posts = require('./posts')
-Write.chats = require('./chats')
-Write.flags = require('./flags')
-Write.admin = require('./admin')
-Write.files = require('./files')
-Write.utilities = require('./utilities')
+Write.users = require('./users');
+Write.groups = require('./groups');
+Write.categories = require('./categories');
+Write.topics = require('./topics');
+Write.posts = require('./posts');
+Write.chats = require('./chats');
+Write.flags = require('./flags');
+Write.admin = require('./admin');
+Write.files = require('./files');
+Write.utilities = require('./utilities');
+Write.career = require('./career');
